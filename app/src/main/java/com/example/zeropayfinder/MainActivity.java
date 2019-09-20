40af22b72fcfd79280b529c641e747f33976e130
chowonsik <cwscms@naver.com>
2019-09-20 23:25:29 +0900

    햄버거 버튼 추가

--- app/src/main/java/com/example/zeropayfinder/MainActivity.java
+++ app/src/main/java/com/example/zeropayfinder/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.zeropayfinder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -24,7 +27,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+//        //액션바 설정하기//
+//        //액션바 타이틀 변경하기
+//        getSupportActionBar().setTitle("ACTIONBAR");
+//        //액션바 배경색 변경
+//        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF339999));
+//        //홈버튼 표시
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//
+//        //액션바 숨기기
+//        //hideActionBar();
 
         mbtMap= (Button) findViewById(R.id.moveMap);
         mbtBook=(Button)findViewById(R.id.moveBook);
@@ -70,10 +82,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     private void moveMap() {
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_nav_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        //or switch문을 이용하면 될듯 하다.
+        if (id == android.R.id.home) {
+            Toast.makeText(this, "홈아이콘 클릭", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (id == R.id.navigation_dashboard) {
+            Toast.makeText(this, "검색 클릭", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (id == R.id.navigation_notifications) {
+            Toast.makeText(this, "액션버튼 클릭", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
