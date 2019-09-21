99c6e3c0532cbdc60d30cce759f1035bb1e3339b
chowonsik <cwscms@naver.com>
2019-09-21 14:57:53 +0900

    제로페이란 설명 추가

--- app/src/main/java/com/example/zeropayfinder/tutorial.java
+++ app/src/main/java/com/example/zeropayfinder/tutorial.java
@@ -2,13 +2,17 @@ package com.example.zeropayfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.webkit.WebView;
 import android.os.Bundle;
 
 public class tutorial extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+
+        webView = (WebView)findViewById(R.id.webview);
+        webView.loadUrl("file:///android_asset/www/index.html");
     }
 }
