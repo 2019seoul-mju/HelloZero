cce2973403fbc90bdb8b20254f307c071f68e182
kangheesu <gmltn4993@naver.com>
2019-09-28 01:09:08 +0900

    스탬프 추가

--- app/src/main/java/com/example/zeropayfinder/stamp.java
+++ app/src/main/java/com/example/zeropayfinder/stamp.java
@@ -17,20 +17,174 @@ public class stamp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stamp);
 
-        final Button button = findViewById(R.id.button6);
+        final Button button1 = findViewById(R.id.button6);
+        final Button button2 = findViewById(R.id.button7);
+        final Button button3 = findViewById(R.id.button8);
+        final Button button4 = findViewById(R.id.button9);
+        final Button button5 = findViewById(R.id.button10);
+
+        final Button button6 = findViewById(R.id.button11);
+        final Button button7 = findViewById(R.id.button12);
+        final Button button8 = findViewById(R.id.button13);
+        final Button button9 = findViewById(R.id.button14);
+        final Button button10 = findViewById(R.id.button15);
 
         //Add OnClickEvents to the button that responds to the user event
-        button.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("test","dfd");
                 if(buttonState % 2 == 0){
-                    button.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                    Toast.makeText(getBaseContext(), "Button background color green", Toast.LENGTH_SHORT).show();
+                    button1.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
                 }
                 else{
-                    button.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-                    Toast.makeText(getBaseContext(), "Button background color accent", Toast.LENGTH_SHORT).show();
+                    button1.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+                }
+                buttonState++;
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test","dfd");
+                if(buttonState % 2 == 0){
+                    button2.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                }
+                else{
+                    button2.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+                }
+                buttonState++;
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test","dfd");
+                if(buttonState % 2 == 0){
+                    button3.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                }
+                else{
+                    button3.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+                }
+                buttonState++;
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test","dfd");
+                if(buttonState % 2 == 0){
+                    button4.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                }
+                else{
+                    button4.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+                }
+                buttonState++;
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test","dfd");
+                if(buttonState % 2 == 0){
+                    button5.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                }
+                else{
+                    button5.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+                }
+                buttonState++;
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test","dfd");
+                if(buttonState % 2 == 0){
+                    button6.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                }
+                else{
+                    button6.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+                }
+                buttonState++;
+            }
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test","dfd");
+                if(buttonState % 2 == 0){
+                    button7.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                }
+                else{
+                    button7.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+                }
+                buttonState++;
+            }
+        });
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test","dfd");
+                if(buttonState % 2 == 0){
+                    button8.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                }
+                else{
+                    button8.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+                }
+                buttonState++;
+            }
+        });
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test","dfd");
+                if(buttonState % 2 == 0){
+                    button9.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                }
+                else{
+                    button9.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+                }
+                buttonState++;
+            }
+        });
+
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test","dfd");
+                if(buttonState % 2 == 0){
+                    button10.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                }
+                else{
+                    button10.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
                 }
                 buttonState++;
             }
